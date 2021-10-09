e5ae4328e36858827952603c3bc9de39f8b812c2
alan <seidihashimura@gmail.com>
2021-10-09 12:43:23 -0300

    [UPD] Adicionando os assets e iniciando o header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import {StatusBar} from "react-native";
+import Logo from "../../assets/logo.svg";
 
 import {
-  Container
+  Container,
+  Header
 } from "./styles";
 
 const Home: React.FC = () => {
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
 
+      </Header>
     </Container>
   );
 };
